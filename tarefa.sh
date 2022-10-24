2bf879361a0e2ef0340336ebb28f071dfe5d8b0b
fat83dotcom <fernando.quimic@gmail.com>
2022-10-24 12:56:06 -0300

    mod servidor

--- tarefa.sh
+++ tarefa.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 #echo "Tarefa Realizada $(date '+%d-%m-%Y %H:%M:%S')" >> logs/log.txt
-source ./venv/bin/activate
+source /home/fernando/Documentos/ServicosWecScrapingDB_Postgres/venv/bin/activate
 python -u "/home/fernando/Documentos/ServicosWecScrapingDB_Postgres/main.py"
