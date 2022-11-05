f264c611587a2ea4aadc2178d7aa77f7bc9d9716
fat83dotcom <fernando.quimic@gmail.com>
2022-11-05 19:08:29 -0300

    Correção de alguns erros relacionados com posicionamento de aspas no texto html que causavam rollback indesejado não atualizando o registro

--- mainG1.py
+++ mainG1.py
@@ -21,21 +21,13 @@ def coreG1():
     for linkMateria in html.select('.menu-item-link'):
         links = linkMateria.get('href')
         if links is not None and (links.count('/') == 4):
-            nomeSessao = linkMateria.select_one('.menu-item-title').get_text().title()
+            nomeSessao = linkMateria.select_one('.menu-item-title').get_text().title().replace("'", '"')
             resposta = requests.get(links)
             html = BeautifulSoup(resposta.text, 'html.parser')
             pkPortal = next(_pkPortal)
             dbPortal.atualizarColuna('dt_hr_pesquisa', f'id_pk={pkPortal}', dataHora)
             dbPortal.atualizarColuna('nome_sessao', f'id_pk={pkPortal}', nomeSessao)
             dbPortal.atualizarColuna('link_site', f'id_pk={pkPortal}', links)
-
-
-            # print(100*'*')
-            # print(pk)
-            # # dbPortal.inserirColunas(f'({next(_pkPortal)})', coluna='(id_pk)')
-            # print(nomeSessao)
-            # print(links)
-            # print(100*'*')
 
             for dadosMateria in html.select('.feed-post-link'):
                 linkMateria = dadosMateria.get('href')
@@ -45,28 +37,18 @@ def coreG1():
                         html = BeautifulSoup(resposta.text, 'html.parser')
                         for materia in html.select('.mc-body'):
                             pkNoticias = f'id_pk={next(_pkNoticias)}'
-                            
-                            # print(next(_pkNoticias))
-                            # dbMaterias.inserirColunas(f'({next(_pkNoticias)})', coluna='(id_pk)')
-                            tituloMateria = materia.select_one('.title').meta.get('content')
+                            tituloMateria = materia.select_one('.title').meta.get('content').replace("'", '"')
                             dataMateria = materia.select_one(
                                 '.content-publication-data__updated').time.get('datetime')
                             textoMateria = materia.find_all('p', class_='content-text__container')
+                            palavras = ''
+                            for palavra in textoMateria:
+                                palavras += f'{str(palavra)} \n'.replace("'", '')
                             dbMaterias.atualizarColuna('referencia_site', pkNoticias, pkPortal)
                             dbMaterias.atualizarColuna('dt_materia', pkNoticias, dataMateria)
                             dbMaterias.atualizarColuna('link_materia', pkNoticias, linkMateria)
                             dbMaterias.atualizarColuna('titulo_materia', pkNoticias, tituloMateria)
-                    
-                            # print(tituloMateria)
-                            # print(dataMateria)
-                            # print(textoMateria)
-                            
-                            palavras = ''
-                            for palavra in textoMateria:
-                                palavras += f'{str(palavra)} \n'
                             dbMaterias.atualizarColuna('texto_materia', pkNoticias, palavras)
-                            # print(palavras)
-                            # print(100*'#')
                     except Exception as erro:
                         print(erro)
     dbPortal.fecharConexao()
