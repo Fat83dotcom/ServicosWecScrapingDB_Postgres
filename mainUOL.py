5ec4caed4ea74524c7b29254469e16bcd5fe6476
fat83dotcom <fernando.quimic@gmail.com>
2022-11-13 15:57:42 -0300

    Preparando e testando as chaves das tabelas para implementação do banco

--- mainUOL.py
+++ mainUOL.py
@@ -6,6 +6,12 @@ from itertools import count
 
 
 def coreUOL():
+    dbLog = OperacoesTabelasBD('"Core_logservicos"')
+    dbPortal = OperacoesTabelasBD('portalUOL')
+    dbMaterias = OperacoesTabelasBD('materiasportalUOL')
+    dataHora: str = str(datetime.now())
+    dbLog.inserirColunas((dataHora, ''), coluna='(dt_hr_exec_func, func_portal)')
+
     url = 'https://noticias.uol.com.br/'
     resposta = requests.get(url)
     html = BeautifulSoup(resposta.text, 'html.parser')
@@ -14,30 +20,35 @@ def coreUOL():
     _pkNoticias = count(0)
     for linksMenuNoticias in html.select('.heading-style'):
         try:
+            pkPortal = next(_pkPortal)
             nomeSessao = linksMenuNoticias.select_one('.custom-title').get_text()
             linkSessao = linksMenuNoticias.a.get('href')
+            print(pkPortal)
+            print(linkSessao)
+            print(100 * '#')
+            # print(nomeSessao)
             resposta = requests.get(linkSessao)
             html = BeautifulSoup(resposta.text, 'html.parser')
-            print(nomeSessao)
-            # print(link)
             for linksMaterias in html.select('.thumbnails-item.align-horizontal'):
                 linkMateria = linksMaterias.a.get('href')
                 resposta = requests.get(linkMateria)
                 html = BeautifulSoup(resposta.text, 'html.parser')
-                # print(link)
+                print(linkMateria)
                 for dadosMateria in html.select('.container.article'):
+                    pkNoticias = next(_pkNoticias)
+                    print(pkNoticias)
+                    print(100 * '*')
                     tituloMateria = dadosMateria.select_one('.custom-title').get_text()
                     dataMateria = dadosMateria.select_one('.p-author.time').get_text().replace('h', ':').strip()[:16]
                     dataMateria = datetime.strptime(dataMateria, '%d/%m/%Y %H:%M')
                     textoMateria = ''
                     for textoCru in dadosMateria.select_one('.text').find_all('p'):
                         textoMateria += textoCru.get_text(' | ', strip=True)
-                    print(dataMateria)
-                    print(tituloMateria)
-                    print(textoMateria)
-        except (AttributeError, TypeError):
+                    # print(dataMateria)
+                    # print(tituloMateria)
+                    # print(textoMateria)
+        except (AttributeError, TypeError, Exception):
             pass
-        
 
 
 coreUOL()
