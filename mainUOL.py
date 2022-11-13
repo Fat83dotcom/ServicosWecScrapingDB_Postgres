620f6e1472061476747ef3448443ea292021a158
fat83dotcom <fernando.quimic@gmail.com>
2022-11-13 00:04:13 -0300

    Desenvolvimento do scraping do portal UOL

--- mainUOL.py
+++ mainUOL.py
@@ -6,15 +6,35 @@ from itertools import count
 
 
 def coreUOL():
-    url = 'https://www.uol.com.br/'
+    url = 'https://noticias.uol.com.br/'
     resposta = requests.get(url)
     html = BeautifulSoup(resposta.text, 'html.parser')
     
-    for linksMenuPrincipal in html.select('.menuDesktop__item'):
-        nomeSessao = linksMenuPrincipal.select_one('.menuDesktop__link__title').get_text().title()
-        link = linksMenuPrincipal.a.get('href')
-        print(nomeSessao)
-        print(link)
+    for linksMenuNoticias in html.select('.heading-style'):
+        try:
+            # print(linksMenuNoticias)
+            nomeSessao = linksMenuNoticias.select_one('.custom-title').get_text()
+            link = linksMenuNoticias.a.get('href')
+            resposta = requests.get(link)
+            html = BeautifulSoup(resposta.text, 'html.parser')
+            print(nomeSessao)
+            # print(link)
+            for linkMaterias in html.select('.thumbnails-item.align-horizontal'):
+                link = linkMaterias.a.get('href')
+                resposta = requests.get(link)
+                html = BeautifulSoup(resposta.text, 'html.parser')
+                # print(link)
+                for dadosMaterias in html.select('.container.article'):
+                    tituloMateria = dadosMaterias.select_one('.custom-title').get_text()
+                    dataMateria = dadosMaterias.select_one('.p-author.time').get_text().replace('h', ':').strip()[:16]
+                    dataMateria = datetime.strptime(dataMateria, '%d/%m/%Y %H:%M')
+                    
+                    print(dataMateria)
+                    print(tituloMateria)
+        except (AttributeError, TypeError):
+            pass
+        print(100 * '*')
+        
 
 
 coreUOL()
