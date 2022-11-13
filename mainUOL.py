e2703ae033489e5fb410fba709f162b5f5a61aa5
fat83dotcom <fernando.quimic@gmail.com>
2022-11-13 00:25:37 -0300

    Desenvolvimento do scraping do portal UOL - Parte I

--- mainUOL.py
+++ mainUOL.py
@@ -10,30 +10,33 @@ def coreUOL():
     resposta = requests.get(url)
     html = BeautifulSoup(resposta.text, 'html.parser')
     
+    _pkPortal = count(0)
+    _pkNoticias = count(0)
     for linksMenuNoticias in html.select('.heading-style'):
         try:
-            # print(linksMenuNoticias)
             nomeSessao = linksMenuNoticias.select_one('.custom-title').get_text()
-            link = linksMenuNoticias.a.get('href')
-            resposta = requests.get(link)
+            linkSessao = linksMenuNoticias.a.get('href')
+            resposta = requests.get(linkSessao)
             html = BeautifulSoup(resposta.text, 'html.parser')
             print(nomeSessao)
             # print(link)
-            for linkMaterias in html.select('.thumbnails-item.align-horizontal'):
-                link = linkMaterias.a.get('href')
-                resposta = requests.get(link)
+            for linksMaterias in html.select('.thumbnails-item.align-horizontal'):
+                linkMateria = linksMaterias.a.get('href')
+                resposta = requests.get(linkMateria)
                 html = BeautifulSoup(resposta.text, 'html.parser')
                 # print(link)
-                for dadosMaterias in html.select('.container.article'):
-                    tituloMateria = dadosMaterias.select_one('.custom-title').get_text()
-                    dataMateria = dadosMaterias.select_one('.p-author.time').get_text().replace('h', ':').strip()[:16]
+                for dadosMateria in html.select('.container.article'):
+                    tituloMateria = dadosMateria.select_one('.custom-title').get_text()
+                    dataMateria = dadosMateria.select_one('.p-author.time').get_text().replace('h', ':').strip()[:16]
                     dataMateria = datetime.strptime(dataMateria, '%d/%m/%Y %H:%M')
-                    
+                    textoMateria = ''
+                    for textoCru in dadosMateria.select_one('.text').find_all('p'):
+                        textoMateria += textoCru.get_text(' | ', strip=True)
                     print(dataMateria)
                     print(tituloMateria)
+                    print(textoMateria)
         except (AttributeError, TypeError):
             pass
-        print(100 * '*')
         
 
 
